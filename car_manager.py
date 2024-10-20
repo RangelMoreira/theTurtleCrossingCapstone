891eb06ec6dbf1e8a97f5a0a59fed72ffde5fcdb
Kevin Rangel Moreira <rangel.moreira@outlook.com>
2024-10-20 10:53:41 -0300

    Creating a project Creating a player and adding movement

--- car_manager.py
+++ car_manager.py
@@ -1,7 +1,26 @@
+import random
+from turtle import Turtle
+import random
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+    def __init__(self, y_position):
+        self.y_position = y_position
+        super().__init__()
+        self.x_position = 250
+        self.shape("square")
+        self.penup()
+        self.shapesize(stretch_wid=2, stretch_len=1)
+        self.color(random.choice(COLORS))
+        self.goto(self.x_position, self.y_position)
+        self.move_speed = 0.1
+        self.left(90)
+
+    def move(self):
+        self.x_position -= STARTING_MOVE_DISTANCE
+        self.goto(self.x_position, self.y_position)
+        
--- main.py
+++ main.py
@@ -1,22 +1,28 @@
+import random
 import time
 from turtle import Screen
 from player import Player
 from car_manager import CarManager
-from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
 game_is_on = True
-
+# car = CarManager(-100)
 player = Player()
 screen.listen()
 screen.onkey(player.move_forward, "Up")
 screen.onkey(player.move_back, 'Down')
+cars = []
 
 while game_is_on:
+
+    random_integer = random.randint(-270, 270)
+    cars.append(CarManager(random_integer))
+    print(random_integer)
     time.sleep(0.1)
     screen.update()
+    cars[0].move()
 
 screen.exitonclick()
